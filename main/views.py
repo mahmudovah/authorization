1206c0c7f7ef49ba09d10868cb28912ad6acbe2e
Hadicha <mahmudovahadija.0831@gmail.com>
2026-03-14 12:46:30 +0500

    first commit

---
+++ main/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+# Create your views here.
+
+def login_view(request: HttpRequest):
+    if request.method == "POST":
+
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if not user:
+            messages.error(request, "username yoki parol xato")
+            return redirect("login")
+        
+        login(request,user)
+        return redirect("login")
+    return render(request, "login.html")
+
+def register_view(request: HttpRequest):
+
+    if request.method == "POST":
+        first_name = request.POST.get("first_name", "Anonim")
+        last_name = request.POST.get("last_name", "Anonim")
+        username = request.POST.get("username")
+
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if User.objects.filter(username=username).exists():
+            messages.warning(request, "Bunday user mavjud")
+            return redirect("register")
+       
+        if not password1 == password2:
+            messages.warning(request, "Parollar mos emas")
+            return redirect("register")
+        
+        user = User.objects.create_user(
+            username=username,
+            password=password1,
+            last_name=last_name,
+            first_name=first_name
+        )
+        return redirect("register")
+    return render(request, "register.html")
